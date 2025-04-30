a7fcbf78d4a3ed9e1cbf1e78f9b573213624533f
larogaming <larogaming2007@gmail.com>
2025-04-30 15:43:12 +0530

    Initial commit

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+const TelegramBot = require('7637293918:AAHSptTosL8dQkr1xEGVZqxm2IlNT5zW0Fg');
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const url = "https://my-server-production-17ad.up.railway.app/";
+
+    bot.sendMessage(chatId, "Click the button below to start the app", {
+        reply_markup: {
+            inline_keyboard: [[
+                { text: "Open App", web_app: { url: url } }
+            ]]
+        }
+    });
+});
